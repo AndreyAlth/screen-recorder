e62555392d1b894a1e40db7820e76f1c240db630
gabriel <andreyalth@gmail.com>
2026-02-23 12:35:34 -0600

    feat: add compiler for windows and mac

--- forge.config.js
+++ forge.config.js
@@ -8,14 +8,27 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
+    // Windows: generates a Squirrel-based .exe installer
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      platforms: ['win32'],
+      config: {
+        name: 'screen_recorder',                        // no spaces or special chars
+        setupExe: 'ScreenRecorderSetup.exe',
+        setupIcon: './src/public/icons/win/icon.ico',   // must be a .ico file
+      },
     },
+    // macOS: generates a .dmg disk image (only builds on macOS)
     {
-      name: '@electron-forge/maker-zip',
+      name: '@electron-forge/maker-dmg',
       platforms: ['darwin'],
+      config: {
+        name: 'Screen Recorder',
+        icon: './src/public/icons/mac/icon.icns',       // must be a .icns file
+        format: 'ULFO',                                 // use 'UDZO' for smaller file size
+      },
     },
+    // Linux: .deb package
     {
       name: '@electron-forge/maker-deb',
       config: {
@@ -24,6 +37,7 @@ module.exports = {
         }
       },
     },
+    // Linux: .rpm package
     {
       name: '@electron-forge/maker-rpm',
       config: {},
